a3a569e1ca822b3cdd8f4e1b91190e0b68623b81
Tomasz Cupiał <me@tomaszcupial.com>
2023-07-09 17:36:35 +0200

    refactor: add props to logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,6 +1,6 @@
 export function Logo(props) {
   return (
-    <div className="text-3xl font-normal">
+    <div className="text-3xl font-normal" {...props}>
       University<span className="text-lime-600 font-bold">Platform</span>
     </div>
   )
